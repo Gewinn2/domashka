8b0aa9020089533e4367a160ceabe9b357dbdbb9
Gewinn2 <daniilpostnickov@yandex.ru>
2024-09-17 10:24:14 +0300

    Добавлены все функции по работе с группой

--- backend/internal/database/group.go
+++ backend/internal/database/group.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type Role struct {
@@ -22,17 +23,107 @@ type GroupUsers struct {
 	RoleId  int `json:"role_id"`
 }
 
-// Вывод группы по id(приглашение и участники)
-func GetGroup(id int, db *sql.DB) {
+// Получение группы, в которой состоит пользователь
+func GetUsersGroup(id int, db *sql.DB) (int, error) {
+	var groupId int
+	query := `SELECT group_id FROM group_users WHERE user_id = $1`
 
+	row := db.QueryRow(query, id)
+	err := row.Scan(&groupId)
+	if err != nil {
+		return 0, fmt.Errorf("GetUsersGroup: %w", err)
+	}
+	return groupId, nil
+}
+
+// Получение участников группы по id
+func GetGroupUsers(id int, db *sql.DB) ([]User, error) {
+	groupId, err := GetUsersGroup(id, db)
+	if err != nil {
+		return nil, fmt.Errorf("GetGroupUsers: %w", err)
+	}
+
+	query := `SELECT user_id FROM group_users WHERE group_id = $1`
+	rows, err := db.Query(query, groupId)
+	if err != nil {
+		return nil, fmt.Errorf("GetGroupUsers: %w", err)
+	}
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var userId int
+		if err := rows.Scan(&userId); err != nil {
+			return nil, err
+		}
+
+		user, err := GetUser(userId, db)
+		if err != nil {
+			return nil, fmt.Errorf("GetGroupUsers: %w", err)
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("GetGroupUsers: %w", err)
+	}
+
+	return users, nil
+}
+
+// Получение инвайта по id пользователя
+func GetGroupLink(id int, db *sql.DB) (string, error) {
+	groupId, err := GetUsersGroup(id, db)
+	if err != nil {
+		return "", err
+	}
+
+	var inviteLink string
+	query := `SELECT invite_link FROM groups WHERE id = $1`
+	row := db.QueryRow(query, groupId)
+	err = row.Scan(&inviteLink)
+	if err != nil {
+		return "", fmt.Errorf("GetGroupLink: %w", err)
+	}
+
+	return inviteLink, nil
 }
 
 // Создание группы
-func CreateGroup() {
-	// TODO: сделать
+func CreateGroup(userId int, g Group, db *sql.DB) (int, error) {
+	var id int
+
+	query := `INSERT INTO groups(group_name, invite_link, created_at) VALUES ($1, $2, $3) RETURNING id`
+	err := db.QueryRow(query, g.GroupName, g.InviteLink, g.CreatedAt).Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("CreateGroup: %w", err)
+	}
+
+	err = JoinToGroup(userId, 0, g.InviteLink, db)
+	if err != nil {
+		return 0, fmt.Errorf("CreateGroup: %w", err)
+	}
+
+	return id, nil
 }
 
 // Присоединение к группе по введенному приглашению
-func JoinToGroup() {
-	// TODO: сделать
+func JoinToGroup(userId, roleId int, inviteLink string, db *sql.DB) error {
+	var groupId int
+	query := `SELECT id FROM groups WHERE invite_link = $1`
+
+	row := db.QueryRow(query, inviteLink)
+	err := row.Scan(&groupId)
+	if err != nil {
+		return fmt.Errorf("JoinToGroup: %w", err)
+	}
+
+	query = `INSERT INTO group_users(user_id, group_id, role_id) VALUES($1, $2, $3)`
+	_, err = db.Exec(query, userId, groupId, roleId)
+	if err != nil {
+		return fmt.Errorf("CreateGroup: %w", err)
+	}
+
+	return nil
 }
--- backend/internal/handler/group.go
+++ backend/internal/handler/group.go
@@ -1,18 +1,93 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"domashka/internal/database"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+	"time"
+)
 
 // Вывод группы по id(приглашение и участники)
 func (s *Server) GetGroup(c *gin.Context) {
-	// TODO: сделать
+	idToConv, ok := c.Get("id")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Id not found"})
+		fmt.Println("GetGroup:", ok)
+		return
+	}
+	id := idToConv.(int)
+
+	users, err := database.GetGroupUsers(id, s.db)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Printf("GetGroup: Empty id field: %s\n", err)
+		return
+	}
+
+	inviteLink, err := database.GetGroupLink(id, s.db)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Printf("GetGroup: Empty id field: %s\n", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"users": users, "invite_link": inviteLink})
 }
 
 // Создание группы
 func (s *Server) CreateGroup(c *gin.Context) {
-	// TODO: сделать
+	idToConv, ok := c.Get("id")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Id not found"})
+		fmt.Println("CreateGroup:", ok)
+		return
+	}
+	id := idToConv.(int)
+
+	var group database.Group
+	if err := c.ShouldBindJSON(&group); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("CreateGroup:", err)
+		return
+	}
+
+	group.InviteLink = uuid.New().String()
+	group.CreatedAt = time.Now().Format("2006-02-01 15:04:05")
+	groupId, err := database.CreateGroup(id, group, s.db)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("CreateGroup:", err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"id": groupId})
 }
 
 // Присоединение к группе по введенному приглашению
 func (s *Server) JoinToGroup(c *gin.Context) {
-	// TODO: сделать
+	idToConv, ok := c.Get("id")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Id not found"})
+		fmt.Println("JoinToGroup:", ok)
+		return
+	}
+	id := idToConv.(int)
+
+	var group database.Group
+	if err := c.ShouldBindJSON(&group); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("JoinToGroup:", err)
+		return
+	}
+
+	err := database.JoinToGroup(id, 0, group.InviteLink, s.db)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("JoinToGroup:", ok)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
